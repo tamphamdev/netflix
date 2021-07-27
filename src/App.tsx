c11e189a6f4691ef85b5df0c6c56258a87476d99
Tam Pham <tampham.dev@gmail.com>
2021-07-27 23:03:42 +0700

    Add accordion container

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumboton";
+import React from "react"
+import { FaqsContainer } from "./containers/faqs"
+import { FooterContainer } from "./containers/footer"
+import { JumbotronContainer } from "./containers/jumboton"
 
 function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,17 +1,58 @@
-import React, { useState } from "react";
-
-export default function Accordion({ children, ...restProps }) {
+import React, { useState, useContext, createContext } from "react"
+import {
+  Container,
+  Frame,
+  Title,
+  Item,
+  Inner,
+  Header,
+  Body,
+} from "../Accordion/styles/accordion"
+type Props = {
+  children: React.ReactNode
+}
+const ToggleContext = createContext()
+export default function Accordion({ children, ...restProps }: Props) {
   return (
     <Container {...restProps}>
       <Inner>{children}</Inner>
     </Container>
-  );
+  )
 }
 
-Accordion.Title = function AccordionTitle({ children, ...restProps }) {
-  return <Title {...re}>{children}</Title>;
-};
+Accordion.Title = function AccordionTitle({ children, ...restProps }: Props) {
+  return <Title {...restProps}>{children}</Title>
+}
 
-Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
-  return <Frame {...re}>{children}</Frame>;
-};
+Accordion.Frame = function AccordionFrame({ children, ...restProps }: Props) {
+  return <Frame {...restProps}>{children}</Frame>
+}
+
+Accordion.Item = function AccordionItem({ children, ...restProps }: Props) {
+  const [togglerShow, setToggleShow] = useState(false)
+
+  return (
+    <ToggleContext.Provider value={{ togglerShow, setToggleShow }}>
+      <Item {...restProps}>{children}</Item>
+    </ToggleContext.Provider>
+  )
+}
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }: Props) {
+  const [toggleShow, setToggleShow] = useState(ToggleContext)
+
+  return (
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}
+      {...restProps}
+    >
+      {children}
+    </Header>
+  )
+}
+
+Accordion.Body = function AccordtionBody({ children, ...restProps }: Props) {
+  const { toggleShow } = useContext(ToggleContext)
+
+  return toggleShow ? <Body {...restProps}>{children}</Body> : null
+}
