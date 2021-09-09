fb76d68853c3691da90e5f15873624ef73d14033
Tam Pham <tampham.dev@gmail.com>
2021-09-09 23:45:51 +0700

    custom hook with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,45 @@
 import React from "react"
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, Switch } from "react-router-dom"
 
 import { Home, Browse, Signin, Signup } from "./pages"
 import * as ROUTES from "./constants/routes"
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes"
+import { useAuthListener } from "./hooks"
 function App() {
-  const user = { name: "Karl" }
+  const { user } = useAuthListener()
+  console.log("user :>> ", user)
   return (
     <Router>
-      <IsUserRedirect
-        user={user}
-        exact
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.SIGN_IN}
-      >
-        <Signin />
-      </IsUserRedirect>
-      <IsUserRedirect
-        user={user}
-        exact
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.SIGN_UP}
-      >
-        <Signup />
-      </IsUserRedirect>
-      {/* <Route path="/user">
-        <p>Hello</p>
-      </Route>
-      <Route path={ROUTES.BROWSE}>
-        <Browse />
-      </Route> */}
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          exact
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          exact
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <Signup />
+        </IsUserRedirect>
 
-      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
-        <Browse />
-      </ProtectedRoute>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+          <Browse />
+        </ProtectedRoute>
 
-      <IsUserRedirect
-        user={user}
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.BROWSE}
-      >
-        <Home />
-      </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.BROWSE}
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   )
 }
