85158f1c716a581eaa487fb4338e46d2820ac11e
Tam Pham <tampham.dev@gmail.com>
2021-09-13 13:56:17 +0700

    Add utils selection filter

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { useContent } from "../hooks"
+import selectionFilter from "../utils/selection-filter"
 
 export default function Browse() {
   const { series } = useContent("series")
   const { films } = useContent("films")
-  console.log("films :>> ", films)
-  console.log(series)
+
+  const slides = selectionFilter({ series, films })
+  console.log(slides)
+
   return <p>Hello browse</p>
 }
