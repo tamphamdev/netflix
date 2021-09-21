790bbe92faa2449059bb736780530c496bf742ec
Tam Pham <tampham.dev@gmail.com>
2021-09-21 20:47:56 +0700

    Add config api

---
+++ src/config.tsx
@@ -0,0 +1,7 @@
+// Configuration for TMDB API
+// Read more about the API here:  https://developers.themoviedb.org/
+
+const API_URL: String = "https://api.themoviedb.org/4/"
+const API_KEY: String | Boolean = import.meta.env.VITE_SOME_KEY || "false"
+
+const
