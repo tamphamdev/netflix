311019978aa8fcc2f0bd24c0ac29a4a7577b5f20
Tam Pham <tampham.dev@gmail.com>
2021-09-22 16:14:04 +0700

    Add helper

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -39,3 +39,26 @@ export function ProtectedRoute({ user, children, ...rest }) {
     />
   )
 }
+
+// Convert time to hours and minutes
+export const calcTime = (time: number): string => {
+  const hours: number = Math.floor(time / 60)
+  const mins: number = time % 60
+
+  return `${hours}h ${mins}m`
+}
+// Convert a number to money formatting
+export const convertMoney = (money: number): string => {
+  const formatter: Intl.NumberFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  })
+  return formatter.format(money)
+}
+
+// Persist state in localstorge
+export const isPersistedState = (stateName: string): any => {
+  const sessionState = sessionStorage.getItem(stateName)
+  return sessionState && JSON.parse(sessionState)
+}
